eae481d2f24df969e3ac24d07c01f26db569ab9b
abraham john <jabrahamjohns@gmail.com>
2024-10-28 02:21:55 +0300

    heme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        primary: '#F97316',  // Orange
+        secondary: '#1E3A8A', // Dark Blue
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
