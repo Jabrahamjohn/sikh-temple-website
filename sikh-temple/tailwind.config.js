711557f93ac57adb6d80cfb55b432f2a6c724370
abraham john <jabrahamjohns@gmail.com>
2024-10-28 03:14:39 +0300

    Refactor tailwind.config.js to extend theme colors

--- sikh-temple/tailwind.config.js
+++ sikh-temple/tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-orange': '#FF7F11',
+        'primary-blue': '#1D3557',
+      },
+    },
   },
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   plugins: [],
 };
+
 
 
