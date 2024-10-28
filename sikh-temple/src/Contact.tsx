c11f6b6eb3ea3efb23d2e494acd552caabf535d6
abraham john <jabrahamjohns@gmail.com>
2024-10-28 03:08:45 +0300

    Add Contact component with contact information

---
+++ sikh-temple/src/Contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <section id="contact" className="py-20 bg-orange-500 text-white">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold">Contact Us</h2>
+        <p className="mt-4">Mombasa Sikh Temple, Nkrumah Road, Mombasa</p>
+        <p>Phone: +254-700-123-456</p>
+        <p>Email: info@mombasatemple.org</p>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
