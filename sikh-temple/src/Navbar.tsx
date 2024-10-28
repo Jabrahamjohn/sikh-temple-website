14388193c5057e0054ee7fc4b34b917a80ff42c4
abraham john <jabrahamjohns@gmail.com>
2024-10-28 03:37:17 +0300

    Refactor Navbar component to update styling and improve accessibility

--- sikh-temple/src/Navbar.tsx
+++ sikh-temple/src/Navbar.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-primary-orange p-4">
-      <ul className="flex justify-around text-white font-semibold">
-        <li><a href="#home" className="hover:text-primary-blue">Home</a></li>
-        <li><a href="#about" className="hover:text-primary-blue">About Us</a></li>
-        <li><a href="#events" className="hover:text-primary-blue">Events</a></li>
-        <li><a href="#contact" className="hover:text-primary-blue">Contact</a></li>
+     <ul className="flex justify-around text-white font-semibold">
+       <li><a href="#home" className="hover:text-primary-blue">Home</a></li>
+       <li><a href="#about" className="hover:text-primary-blue">About Us</a></li>
+       <li><a href="#events" className="hover:text-primary-blue">Events</a></li>
+       <li><a href="#contact" className="hover:text-primary-blue">Contact</a></li>
       </ul>
     </nav>
+
   );
 };
 
